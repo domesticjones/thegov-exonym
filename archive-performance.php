d521f4f08e4ad25623a0e70d8238fa19e5143413
Dustin Jones <info@domesticjones.com>
2019-12-24 04:12:51 -0700

    create logic for performance archive pages

--- archive-performance.php
+++ archive-performance.php
@@ -7,9 +7,6 @@
   get_header();
     echo '<article id="performance-archive" class="page-content">';
 
-
-
-    // Find todays date in Ymd format.
     $dateNow = date('Y-m-d H:i:s');
 
     $upcomingPerfs = get_posts(array(
@@ -32,9 +29,75 @@
       ),
     ));
 
+    $allPerfs = get_posts(array(
+      'post_type'       => 'performance',
+      'posts_per_page'  => -1,
+    ));
+
+    $pastPerfs = get_posts(array(
+      'post_type'       => 'performance',
+      'posts_per_page'  => 999,
+      'meta_query'      => array(
+        'relation'      => 'OR',
+        array(
+          'key'         => 'date_start',
+          'compare'     => '<',
+          'value'       => $dateNow,
+          'type'        => 'DATETIME',
+        ),
+        array(
+          'key'         => 'date_end',
+          'compare'     => '<',
+          'value'       => $dateNow,
+          'type'        => 'DATETIME',
+        ),
+      ),
+    ));
+
+
+
     if($upcomingPerfs) {
-      foreach($upcomingPerfs as $perf) {
-        the_title();
+      ex_wrap('start', 'perf-upcoming');
+        foreach($upcomingPerfs as $perf) {
+          $date       = get_field('date');
+          $title = get_the_title($perf->ID);
+          $poster = get_the_post_thumbnail($perf->ID, 'small');
+          $location   = get_field('location');
+        }
+      ex_wrap('end');
+    }
+
+
+
+    $otherActs = [];
+    if($allPerfs) {
+      foreach($allPerfs as $perf) {
+        $acts = get_field('acts', $perf->ID);
+        if($acts) {
+          foreach($acts as $act) {
+            array_push($otherActs, $act['name']);
+          }
+        }
+      }
+    }
+    $actsList = array_unique($otherActs);
+    sort($actsList);
+    if($actsList) {
+      ex_wrap('start', 'perf-otheracts');
+        echo '<h2>' . ex_brand() . ' has performed with the following acts:</h2>';
+        echo '<ul>';
+          foreach($actsList as $act) {
+            echo '<li>' . $act . '</li>';
+          }
+        echo '</ul>';
+      ex_wrap('end');
+    }
+
+
+
+    if($pastPerfs) {
+      foreach($pastPerfs as $perf) {
+        //echo get_the_title($perf->ID);
       }
     }
 
