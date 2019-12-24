1b4b2cd2d1b45ed5c33fa3fb64791ff2ba56a220
Dustin Jones <info@domesticjones.com>
2019-12-23 23:42:18 -0700

    Style performance page, header, and mobile nav down to phone size

--- modules/navigation.php
+++ modules/navigation.php
@@ -4,13 +4,17 @@
       <?php
         $navMenu = wp_get_nav_menu_items('primary-menu');
         foreach($navMenu as $nav) {
-          $navMeta = get_post_meta($nav->ID);
-          $nameShort = $navMeta['short_name'][0];
-          $nameLong = $nav->title;
+          $navMeta    = get_post_meta($nav->ID);
+          //var_dump($nav);
+          $nameShort  = $navMeta['short_name'][0];
+          $nameLong   = $nav->title;
+          $link       = $nav->url;
           echo '<li>';
-          $icon = (int)$navMeta['icon'][0];
-            echo wp_get_attachment_image($icon);
-            echo '<a href=""><span>' . $nameLong . '</span><i>' . $nameShort . '</i></a>';
+            echo '<a href="' . $link . '">';
+            $icon = (int)$navMeta['icon'][0];
+              echo wp_get_attachment_image($icon);
+              echo '<p><span>' . $nameLong . '</span><i>' . $nameShort . '</i></p>';
+            echo '</a>';
           echo '</li>';
         }
       ?>
